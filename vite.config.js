24332261a1423b0ce7b2bfb0b27b11d856c58738
Guillaume Regimbeau <gregimbeau@mail.com>
2023-07-31 17:48:41 +0200

    git pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// Get the repository name from the package.json file
-const pkg = require("./package.json");
-const repoName = pkg.name;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -14,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: `/${S5J1}/`, // Set the base property to '/<REPO>/'
+  base: "/S5J1/", // Set the base property to '/S5J1/'
 });
